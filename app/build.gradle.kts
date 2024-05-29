eda77be2af5379c965a57f198ecac507f0140faf
joan-15 <159147082+joan-15@users.noreply.github.com>
2024-05-29 08:40:35 -0500

    se congelo el programa

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,11 +36,9 @@ android {
 }
 
 dependencies {
-
-    implementation ("com.airbnb.android:lottie:6.4.0")
-
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
+    implementation("com.airbnb.android:lottie:6.4.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
